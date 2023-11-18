44c3366eff0581d43e30797349475aea82bd6ea0
Leroy Salih <leroy.salih@gmail.com>
2023-11-18 14:57:51 +0000

    Fixed week labels

--- app/(dev)/homework-view/display-assignment/index.tsx
+++ app/(dev)/homework-view/display-assignment/index.tsx
@@ -3,11 +3,25 @@ import parseISO from 'date-fns/parseISO'
 
 import styles from './display-assignment.module.css'
 
+
+const formatWeeksDistance = (dueDateTime: string) => {
+    
+    const diff = differenceInWeeks(startOfWeek(parseISO(dueDateTime)), startOfWeek(Date.now()) );
+
+    if (diff == 0) return 'Current';
+    if (diff == 1) return `In ${diff} week`;
+    if (diff == -1) return `1 week ago`;
+    if (diff > 1) return  `In ${diff} weeks`;
+    if (diff < -1) return  `${-diff} weeks ago`;
+    
+}
+
 export default function DisplayAssignment ({title, status, dueDateTime}: {title: string, status: string, dueDateTime: string}) {
     return <div className={styles.cell}>
         <div className={styles.title}>{title}</div>
         <div className={styles.subHeader}>
-            <div className={styles.dueDateTime}>{format(parseISO(dueDateTime), "yyyy-MM-dd")}</div>
+            <div className={styles.dueDateTime}>For Week: {format(startOfWeek(parseISO(dueDateTime)), "yyyy-MM-dd")}</div>
+            <div className={styles.dueDateTime}>{ formatWeeksDistance(dueDateTime) }</div>
             <div className={styles.status}>{status}</div>
         </div>
     </div>
