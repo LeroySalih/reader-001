f3ffd457babea31801bf6e98392336ff95b8b0bb
Leroy Salih <leroy.salih@gmail.com>
2024-05-10 15:18:07 +0000

    Added refresh

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         serverActions: {
             allowedOrigins: [
                 "localhost:3000", 
-                "reader-001.vercel.app/",
+                "reader-001.vercel.app",
                 "silver-xylophone-975xqrwvp42qww-3000.app.github.dev"],
         },
     },
